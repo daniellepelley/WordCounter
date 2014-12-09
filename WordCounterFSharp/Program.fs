caf33230eedbe1b35bbbc041a9385a6a8e3c054c
Daniel Le Pelley <daniellepelley@gmail.com>
2014-12-09 07:17:33 +0000

    Got FSharp working

--- WordCounterFSharp/Program.fs
+++ WordCounterFSharp/Program.fs
@@ -1,11 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.net
+open System
 open System.IO
 
+
 let CountWords file = 
+    let nonLetters = [0..255]
+                        |> Seq.map(fun x -> Convert.ToChar x)
+                        |> Seq.filter(fun x -> Char.IsLetter(x) <> true)
+                        |> Seq.toArray
     seq { use fileReader = new StreamReader(File.OpenRead(file))
         while not fileReader.EndOfStream do
             yield fileReader.ReadLine() }
-        |> Seq.collect (fun line -> line.Split [|' '|])
+        |> Seq.collect (fun line -> line.Split nonLetters)
         |> Seq.countBy (fun x -> x)
         |> Seq.toArray
 
